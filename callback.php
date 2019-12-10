88d5422b75e283e374ec58ea884a156b859b76e9
SuperSonic <supersonic@livemail.tw>
2019-12-10 14:49:13 +0000

    Add modules

---
+++ callback.php
@@ -0,0 +1,31 @@
+<?php
+/*
+    PB Project Demo - LINEBOT
+    :license MPL 2.0
+    (c) 2020 SuperSonic(https://github.com/supersonictw)
+*/
+
+require_once('api/api.php');
+
+include('set.php');
+
+$client = new LINEAPI($channelAccessToken, $channelSecret);
+$msgobj = new LINEMSG();
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage($event['replyToken'], $msgobj->Text($message['text']));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
