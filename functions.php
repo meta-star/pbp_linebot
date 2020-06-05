e3cff3f4c769639d9da1691642da269544a4d189
SuperSonic <supersonic@livemail.tw>
2020-06-05 04:03:23 +0000

    Fix reporting bug

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ function analytics($message_text)
     }
 
     if (in_array(100, $results) or in_array(200, $results)) {
-        if (min($result) >= 100) {
+        if (count($results) == 1 or min($result) >= 100) {
             return ltrim($ext_msg);
         }
     }
